f95d12dc725427cbc1ee37ebd2b375f0aa94881e
Alex Guo <alexguoxh@gmail.com>
2024-11-12 18:08:51 -0500

    Added commands to switch between evaluation and differentiation modes

--- main.cpp
+++ main.cpp
@@ -11,25 +11,60 @@ using std::endl;
 using std::vector;
 using std::unique_ptr;
 
+enum class Mode{EVAL, DIFF};
+
+void help() {
+    cout << "Commands: " << endl;
+    cout << "Evaluation mode: /e" << endl;
+    cout << "Differentiate mode: /d <wrt>" << endl;
+    cout << "Help: /h" << endl;
+    cout << "Quit: /q" << endl;
+}
+
+void header(Mode m, char wrt) {
+    if (m == Mode::DIFF) {
+        cout << "Differentiation mode (wrt " << wrt << "):" << endl;
+    } else {
+        cout << "Evaluation mode:" << endl;
+    }
+}
+
 int main(int argc, char** argv) {
     string expression;
+    Mode mode = Mode::EVAL;
+    char wrt = 'x';
 
-    cout << "Enter an expression (q to quit):" << endl;
+    help(); 
+
     while (std::getline(cin, expression)) {
-
-        if (expression == "q") {
+        if (expression == "/q") {
             break;
+        } else if (expression == "/h") {
+            help();
+            continue;
+        } else if (expression == "/e") {
+            mode = Mode::EVAL;
+            header(mode, wrt);
+            continue;
+        } else if (expression.substr(0, 2) == "/d") {
+            mode = Mode::DIFF;
+            wrt = expression.length() > 3 ? expression[3] : 'x';
+            header(mode, wrt);
+            continue;
         }
 
         vector<Token> tokens = tokenize(expression);
 
         if (! tokens.empty()) {
             unique_ptr<NodeBase> node = buildTree(tokens);
-            // cout << "= " << node->evaluate() << endl;
-            cout << node->clone()->differentiate('x')->toString() << endl;
+            if (mode == Mode::EVAL) {
+                cout << "= " << node->evaluate() << endl;
+            } else if (mode == Mode::DIFF) {
+                cout << "d/d" << wrt << "(" << expression << ") = " << node->differentiate(wrt)->toString() << endl;
+            }
         }
 
-        cout << "Enter an expression (q to quit):" << endl;
+        header(mode, wrt);
     }
 
     return 0;
